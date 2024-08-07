8fe482e9c48efe64b7cb25cffd6d2bfff3ac19f2
Rik Volger <rvolger@gmail.com>
2024-08-07 17:05:35 +0200

    Allow for other storage filetypes

--- scripts/pathbuilders.py
+++ scripts/pathbuilders.py
@@ -12,21 +12,25 @@ def concpathbuilder(root, concentration, flowrate, framerate):
 
 
 def fullpathbuilder(root, concentration, framerate):
-    return pathbuilder(root, ["preprocessed", concentration, "0lmin", framerate])
+    return pathbuilder(root, ["preprocessed", concentration, "Full", framerate])
 
 
 def emptypathbuilder(root, framerate):
-    return pathbuilder(root, ["preprocessed", "Empty", framerate])
+    # return pathbuilder(root, ["preprocessed", "Empty", framerate])
+    return pathbuilder(root, ["preprocessed", "single-source", "Empty", framerate])
 
 
-def mat_filename(
+def extensionless_filename(
         loss: bool = False,
         volume: bool = False,
         resolution: bool = False,
         mask: bool = False,
+        bhc: bool = False,
+        init: str = "flat",
         recon_size: dict = None,
         voxel_size: float = None,
         mask_size: float = None) -> str:
+    """Build filename for the requested savefile."""
     filenameparts = ["recon"]
     if loss:
         filenameparts += ["loss"]
@@ -36,7 +40,85 @@ def mat_filename(
         filenameparts += [f"resolution-{voxel_size}cm"]
     if mask:
         filenameparts += [f"mask-{mask_size}cm"]
+    if bhc:
+        filenameparts += ["bh-corrected"]
+    if init != "flat":
+        filenameparts += [f"init-{init}"]
 
-    filename = "_".join(filenameparts) + ".mat"
+    filename = "_".join(filenameparts)
 
     return filename
+
+
+def mat_filename(
+        loss: bool = False,
+        volume: bool = False,
+        resolution: bool = False,
+        mask: bool = False,
+        bhc: bool = False,
+        init: str = "flat",
+        recon_size: dict = None,
+        voxel_size: float = None,
+        mask_size: float = None) -> str:
+    """Create filename with mat extension for savemat. Relies mainly on `extensionless_filename()`"""
+    filename = extensionless_filename(
+        loss,
+        volume,
+        resolution,
+        mask,
+        bhc,
+        recon_size,
+        voxel_size,
+        mask_size) + ".mat"
+
+    return filename
+
+
+def pkl_filename(
+        loss: bool = False,
+        volume: bool = False,
+        resolution: bool = False,
+        mask: bool = False,
+        bhc: bool = False,
+        init: str = "flat",
+        recon_size: dict = None,
+        voxel_size: float = None,
+        mask_size: float = None) -> str:
+    """Create filename with pkl extension for pickle. Relies mainly on `extensionless_filename()`"""
+    filename = extensionless_filename(
+        loss,
+        volume,
+        resolution,
+        mask,
+        bhc,
+        recon_size,
+        voxel_size,
+        mask_size) + ".pkl"
+
+    return filename
+
+
+def hdf5_filename(
+        loss: bool = False,
+        volume: bool = False,
+        resolution: bool = False,
+        mask: bool = False,
+        bhc: bool = False,
+        init: str = "flat",
+        recon_size: dict = None,
+        voxel_size: float = None,
+        mask_size: float = None) -> str:
+    """Create filename with hdf5 extension. Relies mainly on `extensionless_filename()`"""
+    filename = extensionless_filename(
+        loss,
+        volume,
+        resolution,
+        mask,
+        bhc,
+        init,
+        recon_size,
+        voxel_size,
+        mask_size) + ".hdf5"
+
+    return filename
+
